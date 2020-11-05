a3cf0346192f1e9e5239f5360f3020ed1e878d1b
ObitoRin <2209246794@qq.com>
2020-11-06 03:30:59 +0800

    提交项目

--- src/main.js
+++ src/main.js
@@ -1,12 +1,21 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
-import store from './store'
+import Vuex from 'vuex'
+import store from './store/index'
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+import util from './helpers/util'
 
+Vue.use(ElementUI)
+Vue.use(Vuex)
+Vue.use(util)
 Vue.config.productionTip = false
+
+window.store = store
 
 new Vue({
   router,
   store,
-  render: h => h(App)
+  render: function (h) { return h(App) }
 }).$mount('#app')
--- src/router/index.js
+++ src/router/index.js
@@ -1,27 +1,68 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import store from '../store'
 
 Vue.use(VueRouter)
 
-const routes = [
-  {
-    path: '/',
-    name: 'Home',
-    component: Home
-  },
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
-]
+//懒加载
+const router =  new VueRouter({
+  routes: [
+    {
+      path: '/',
+      component: () => import('@/views/Index/template.vue')
+    },
+    {
+      path: '/login',
+      component: () => import('@/views/Login/template.vue')
+    },
+    {
+      path: '/detail/:blogId',
+      component: () => import('@/views/Detail/template.vue')
+    },
+    {
+      path: '/edit/:blogId',
+      component: () => import('@/views/Edit/template.vue'),
+      meta: { requiresAuth: true }
+    },
+    {
+      path: '/create',
+      component: () => import('@/views/Create/template.vue'),
+      meta: { requiresAuth: true }//登录后才能访问的组件
+    },
+    {
+      path: '/user/:userId',
+      component: () => import('@/views/User/template.vue')
+    },
+    {
+      path: '/my',
+      component: () => import('@/views/My/template.vue'),
+      meta: { requiresAuth: true }
+    },
+    {
+      path: '/register',
+      component: () => import('@/views/Register/template.vue')
+    }
+  ]
+})
 
-const router = new VueRouter({
-  routes
+// 导航守卫  路由创建之前
+router.beforeEach((to, from, next) => {
+  if (to.matched.some(record => record.meta.requiresAuth)) {
+    // 判断用户是否登录  没登录就跳转到登录页面  登录了就继续执行下去
+    store.dispatch('checkLogin').then(isLogin=>{
+      if (!isLogin) {
+        next({
+          path: '/login',
+          query: { redirect: to.fullPath }
+        })
+      } else {
+        next()
+      }    
+    })
+  } else {
+    next()
+  }
 })
 
 export default router
+ 
